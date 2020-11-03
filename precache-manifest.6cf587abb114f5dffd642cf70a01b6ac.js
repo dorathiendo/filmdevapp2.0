7b8ba59f7bcefd8266016addf54dcce22e2697e2
Dora Do <>
2020-11-03 15:03:54 -0800

    Updates

---
+++ precache-manifest.6cf587abb114f5dffd642cf70a01b6ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949266e3ffb8316695a839d99633cf0a",
+    "url": "/filmdevapp2.0/index.html"
+  },
+  {
+    "revision": "85fb1be16b3bf9178223",
+    "url": "/filmdevapp2.0/static/css/main.69abf241.chunk.css"
+  },
+  {
+    "revision": "ddd9bec1a11c32e9f8b1",
+    "url": "/filmdevapp2.0/static/js/2.58965936.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/filmdevapp2.0/static/js/2.58965936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fb1be16b3bf9178223",
+    "url": "/filmdevapp2.0/static/js/main.f6f527ed.chunk.js"
+  },
+  {
+    "revision": "2a5a396a59bcd9491796",
+    "url": "/filmdevapp2.0/static/js/runtime-main.b29293d1.js"
+  }
+]);
